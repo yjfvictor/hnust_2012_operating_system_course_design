da34af6e25a426394ce30659ed9e191e678a7891
yejianfei <jianfeiye@acm.org>
2014-06-13 01:11:33 +0800

    实验一  清单1-1的正确代码

---
+++ 1/1-1.cpp
@@ -0,0 +1,17 @@
+/*
+ *
+ * 实验一  清单1-1的正确代码
+ *
+ *             叶剑飞
+ *            2014年6月13日
+ *
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+	std::cout << "Hello, Win32 Console Application" << std::endl;
+	return EXIT_SUCCESS;
+}
