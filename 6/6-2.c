fc74bfb231369a9776b1efa2cfecf47594f88eff
yejianfei <jianfeiye@acm.org>
2014-06-15 00:33:44 +0800

    增加6-2的代码注释

--- 6/6-2.c
+++ 6/6-2.c
@@ -16,14 +16,18 @@
 #include <sys/shm.h>
 
 // 消息的最长的长度
-#define MAX_LENGTH 100
+#define MAX_LENGTH 8
 
 // 按照指导书上的要求，建立key为75的消息队列
 const key_t key = 75;
 
 void server(void)
 {
+	// 进程间共享内存的映射区
 	volatile char * shmaddr;
+	// 这里使用volatile是避免编译器优化
+	// 因为它的修改是来自另一个进程的进程的修改
+	// 如果不用volatile，没做“左值”(lvalue)的代码段可能会被编译器优化到寄存器中
 
 	// IPC_CREAT表示如果不存在这个key值的消息队列，则创建它
 	// 八进制600，表示当前用户有读写权限
@@ -40,6 +44,7 @@ void server(void)
 		exit(EXIT_FAILURE);
 	}
 
+	// 把进程间共享内存映射到进程内存中
 	shmflg = 0;
 	shmaddr = (volatile char *)shmat(shmid, NULL, shmflg);
 	if ( shmaddr == (void *)(-1) )
@@ -50,7 +55,10 @@ void server(void)
 
 	do
 	{
+		// 向共享内存中写入数据
 		shmaddr[0] = (char)(-1);
+
+		// 等待共享内存中的数据发生改变
 		while ( shmaddr[0] == (char)(-1) )
 		{ }
 		puts("(server) received");
@@ -59,8 +67,8 @@ void server(void)
 
 void client(void)
 {
-	int i;
 	volatile char * shmaddr;
+	int i;  // 循环计数
 	int shmflg = IPC_CREAT | 0600;
 	size_t size = MAX_LENGTH;
 	int shmid = shmget(key, size, shmflg);
@@ -81,8 +89,10 @@ void client(void)
 
 	for ( i = 9; i >= 0; -- i )
 	{
+		// 等待共享内存中的数据发生改变
 		while ( shmaddr[0] != (char)(-1) )
 		{ }
+		// 向共享内存中写入数据
 		shmaddr[0] = i;
 		puts("(client) sent");
 	}
